81b1d93a7bf30bce4f518a6b8521f5a422b048f5
evansmj <evansmj500@gmail.com>
2023-11-26 13:15:30 -0500

    Zoomable chart
    Use material theme for ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,22 @@
   import * as d3 from 'd3'
   import { pageViewModel } from './PageViewModel'
   import type { GraphData, Link, Node } from '../network/GraphData'
+  import Banner, { Label, CloseReason } from '@smui/banner'
+  import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar'
+  import Textfield, { Input } from '@smui/textfield'
+  import Icon from '@smui/textfield/icon'
+  import HelperText from '@smui/textfield/helper-text'
+  import Button from '@smui/button'
 
-  let address: string
-  let rune: string
+  let address: string = ''
+  let rune: string = ''
   let svg: any
+  let g: any
 
   let connectionStatus = pageViewModel.getConnectionStatus()
   let graphData: GraphData
+
+  let nodeRadius = 45
 
   $: if (graphData !== undefined) {
     updateGraph(graphData)
@@ -23,13 +32,30 @@
     console.log('onMount() pageViewModel ' + pageViewModel)
 
     // append the svg object to the body of the page
-    svg = d3
-      .selectAll('div.chart')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    svg = d3.selectAll('div.content').append('svg').attr('width', '100%').attr('height', '100%')
+
+    g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+    svg.call(d3.zoom().on("zoom", (event: { transform: any }) => {
+      g.attr("transform", event.transform)
+    }))
+
+    svg
+      .append('defs')
+      .selectAll('marker')
+      .data(['end'])
+      .enter()
+      .append('marker')
+      .attr('id', 'end')
+      .attr('viewBox', '0 -5 10 10')
+      .attr('refX', 0)
+      .attr('refY', 0)
+      .attr('markerWidth', 6)
+      .attr('markerHeight', 6)
+      .attr('orient', 'auto')
+      .append('path')
+      .attr('d', 'M0, -5L10,0L0,5')
+      .attr('fill', '#aaa')
 
     let graphDataObserver = {
       next: (data: GraphData | null) => {
@@ -52,15 +78,33 @@
 
   function updateGraph(data: GraphData) {
     console.log('updateGraph ' + data.nodes.length)
-    var link = svg.selectAll('line').data(data.links).enter().append('line').style('stroke', '#aaa')
-
-    var node = svg
-      .selectAll('circle')
-      .data(data.nodes)
+    var link = g
+      .selectAll('line')
+      .data(data.links)
       .enter()
-      .append('circle')
-      .attr('r', 20)
-      .style('fill', '#69b3a2')
+      .append('path')
+      .attr('class', function (d) {
+        return 'link-source=-' + d.source.id + ' target-' + d.target.id
+      })
+      .attr('d', pathD)
+      //.append('line')
+      .attr('fill', 'none')
+      .style('stroke', '#aaa')
+      .attr('marker-end', 'url(#end)')
+
+    var node = g.selectAll('.node').data(data.nodes).enter().append('g').attr('class', 'node')
+
+    node.append('circle').attr('r', nodeRadius).style('fill', '#69b3a2')
+
+    node
+      .append('text')
+      .text(function (d: Node) {
+        return d.name.substring(0, 4)
+      })
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.35em')
+      .style('font-size', '10px')
+      .attr('fill', '#000')
 
     var simulation = d3
       .forceSimulation(data.nodes)
@@ -75,29 +119,32 @@
       )
       .force('charge', d3.forceManyBody().strength(-400))
       .force('center', d3.forceCenter(width / 2, height / 2))
-      .on('end', ticked)
+      .force('collide', d3.forceCollide(120))
+      .on('tick', ticked)
 
     function ticked() {
-      link
-        .attr('x1', function (d) {
-          return d.source.x
-        })
-        .attr('y1', function (d) {
-          return d.source.y
-        })
-        .attr('x2', function (d) {
-          return d.target.x
-        })
-        .attr('y2', function (d) {
-          return d.target.y
-        })
-      node
-        .attr('cx', function (d) {
-          return d.x + 6
-        })
-        .attr('cy', function (d) {
-          return d.y - 6
-        })
+      link.attr('d', pathD)
+
+      node.attr('transform', function (d) {
+        return 'translate(' + d.x + ',' + d.y + ')'
+      })
+    }
+
+    function pathD(d) {
+      const dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy) * 1.3, // Add a multiplier to make dr longer
+        angle0 = Math.atan2(dy, dx),
+        //nodeRadius = 45, // Your node radius
+        markerWidth = 6, // Your marker width
+        radius = nodeRadius + markerWidth
+
+      const x1 = d.source.x + Math.cos(angle0) * nodeRadius,
+        y1 = d.source.y + Math.sin(angle0) * nodeRadius,
+        x2 = d.target.x - Math.cos(angle0) * radius,
+        y2 = d.target.y - Math.sin(angle0) * radius
+
+      return `M${x1},${y1}A${dr},${dr} 0 0,1 ${x2},${y2}`
     }
   }
 
@@ -106,65 +153,126 @@
   }
 </script>
 
-<main class="w-screen flex items-center justify-center p-6 relative">
+<main class="flex flex-col h-screen">
   <!--{#if ln}
     <div class="absolute top-1 right-1 px-2 py-1 border-green-600 rounded border text-sm">
       Browser Id: {`${ln.publicKey.slice(0, 8)}...${ln.publicKey.slice(-8)}`}
     </div>
   {/if}-->
+  <TopAppBar class="z-50 customTopBar" variant="static">
+    <Row>
+      <Section>
+        <Title>Lightning Channel Visualizer</Title>
+      </Section>
+    </Row>
+  </TopAppBar>
 
-  <div class="w-1/2 max-w-lg">
-    <h1 class="font-bold text-3xl mb-4 w-full text-center">CLN Node Visualizer</h1>
-    <div class="w-full mt-4 text-sm p-4 border-2 rounded border-purple-300">
-      <label class="text-neutral-600 font-medium mb-1 block" for="address">Address</label>
-      <textarea
-        id="address"
-        class="border w-full p-2 rounded"
-        rows="3"
-        bind:value={address}
-        placeholder="033f4bbfcd67bd0fc858499929a3255d063999ee23f4c5e12b8b1089e132b3e408@localhost:7272"
-      />
+  <div class="content flex-grow overflow-hidden" />
 
-      <label class="text-neutral-600 font-medium mb-1 block" for="rune">Rune</label>
-      <textarea
-        id="rune"
-        class="border w-full p-2 rounded"
-        rows="2"
-        bind:value={rune}
-        placeholder="O2osJxV-6lGUgAf-0NllduniYbq1Zkn-45trtbx4qAE9MA=="
-      />
+  <!--div class="w-full mt-4 text-sm p-4 border-2 rounded border-purple-300">
+    <label class="text-neutral-600 font-medium mb-1 block" for="address">Address</label>
+    <textarea
+      id="address"
+      class="border w-full p-2 rounded"
+      rows="3"
+      bind:value={address}
+      placeholder="033f4bbfcd67bd0fc858499929a3255d063999ee23f4c5e12b8b1089e132b3e408@localhost:7272"
+    />
 
-      <div class="flex items-center justify-between w-full">
-        <button
-          on:click={executeConnect}
-          disabled={!address}
-          class="mt-2 border border-purple-500 rounded py-1 px-4 disabled:opacity-20 hover:shadow-md active:shadow-none"
-          >Connect</button
+    <label class="text-neutral-600 font-medium mb-1 block" for="rune">Rune</label>
+    <textarea
+      id="rune"
+      class="border w-full p-2 rounded"
+      rows="2"
+      bind:value={rune}
+      placeholder="O2osJxV-6lGUgAf-0NllduniYbq1Zkn-45trtbx4qAE9MA=="
+    />
+
+    <div class="flex items-center justify-between w-full">
+      <button
+        on:click={executeConnect}
+        disabled={!address}
+        class="mt-2 border border-purple-500 rounded py-1 px-4 disabled:opacity-20 hover:shadow-md active:shadow-none"
+        >Connect</button
+      >
+
+      {#if $connectionStatus}
+        <div class="flex items-center">
+          <div class="text-sm">{$connectionStatus}</div>
+          <div
+            class:bg-green-500={$connectionStatus === 'connected'}
+            class:bg-yellow-500={$connectionStatus === 'connecting' ||
+              $connectionStatus === 'waiting_reconnect'}
+            class:bg-red-500={$connectionStatus === 'disconnected'}
+            class="w-3 h-3 rounded-full ml-1 transition-colors"
+          />
+        </div>
+      {/if}
+    </div>
+  </div-->
+
+  <div
+    class="footer pl-4 pr-4 pt-4 pb-4 bottom-0 w-screen flex flex-row justify-between items-center"
+  >
+    <div>
+      <Textfield variant="standard" bind:value={address} label="Address">
+        <HelperText persistent slot="helper"
+          >033f4bbfcd67bd0fc858499929a3255d063999ee23f4c5e12b8b1089e132b3e408@localhost:7272</HelperText
         >
-
-        {#if $connectionStatus}
-          <div class="flex items-center">
-            <div class="text-sm">{$connectionStatus}</div>
-            <div
-              class:bg-green-500={$connectionStatus === 'connected'}
-              class:bg-yellow-500={$connectionStatus === 'connecting' ||
-                $connectionStatus === 'waiting_reconnect'}
-              class:bg-red-500={$connectionStatus === 'disconnected'}
-              class="w-3 h-3 rounded-full ml-1 transition-colors"
-            />
-          </div>
-        {/if}
-      </div>
+      </Textfield>
+    </div>
+    <div>
+      <Textfield variant="standard" bind:value={rune} label="Rune">
+        <HelperText persistent slot="helper"
+          >O2osJxV-6lGUgAf-0NllduniYbq1Zkn-45trtbx4qAE9MA==</HelperText
+        >
+      </Textfield>
+    </div>
+    <div>
+      <Button
+        class="load-button"
+        variant="raised"
+        on:click={() => executeConnect()}
+        disabled={!address || !rune}
+      >
+        <Label class="font-bold">Load</Label>
+      </Button>
     </div>
   </div>
 </main>
 
-<div class="w-full flex justify-center mt-auto">
+<!--div class="w-full flex justify-center mt-auto">
   <div class="p-4 border-2 rounded border-green-300">
-    <div id="d3-container" style="width: 800px; height:800px">
-      <div class="pb-40">
-        <div class="chart" />
-      </div>
+    <div id="d3-container" style="width: 800px; height:1800px pb-40">
+      <div class="chart" />
     </div>
   </div>
-</div>
+</div-->
+
+<style>
+  main {
+    font-family: 'Courier New', Courier, monospace;
+  }
+
+  :global(.mdc-top-app-bar__title) {
+    color: #ffffff;
+  }
+
+  .content {
+    background-color: #ffffff;
+  }
+
+  :global(.mdc-text-field) {
+    width: 50rem;
+  }
+
+  .footer {
+    background-color: #ffffff;
+    color: #ffffff;
+  }
+
+  :global(.mdc-button) {
+    color: #ffffff;
+    --mdc-theme-on-primary: #ffffff;
+  }
+</style>
