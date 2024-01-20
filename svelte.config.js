ee9bd5dec1b134b56226b97ab5cf155b9a80a8c2
isaac <isaacyampo@gmail.com>
2024-01-19 23:34:55 -0600

    Change Adapter and Disable GHP Jekyll generator
    Changed deployment adapter to a crowdsourced GH Pages (GPH) adapter from Svelte website and added file to disable Jekyll static site generation on GHP

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-github';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
